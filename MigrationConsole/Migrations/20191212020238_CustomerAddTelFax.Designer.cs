c7d0aef265fbef27db141d0641d7331f2742bc93
Kuei Peng <ragnakuei@livemail.tw>
2019-12-12 10:09:42 +0800

    AddCustomerTexFax

--- MigrationConsole/Migrations/20191212020238_CustomerAddTelFax.Designer.cs
+++ MigrationConsole/Migrations/20191212020238_CustomerAddTelFax.Designer.cs
@@ -3,14 +3,16 @@ using EfCoreFirstTest;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MigrationConsole.Migrations
 {
     [DbContext(typeof(TestDbContext))]
-    partial class TestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191212020238_CustomerAddTelFax")]
+    partial class CustomerAddTelFax
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MigrationConsole/Migrations/20191212020238_CustomerAddTelFax.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MigrationConsole.Migrations
+{
+    public partial class CustomerAddTelFax : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Fax",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tel",
+                table: "Customers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fax",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Tel",
+                table: "Customers");
+        }
+    }
+}
